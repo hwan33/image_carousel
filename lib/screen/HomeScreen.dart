fa618ace8f9919a5f8fc6fccc4297f73eb1b29dd
권용환 <hahahaa_a@naver.com>
2023-04-21 23:24:02 +0900

    타이머 추가하기 - Timer 등록

--- lib/screen/HomeScreen.dart
+++ lib/screen/HomeScreen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -12,17 +14,30 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
 
   @override
+  void initState() {
+    super.initState();
+
+    Timer.periodic(
+      Duration(seconds: 3),
+          (timer) {
+        print('실행!');
+      },
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
     return Scaffold(
       body: PageView(
         children: [1, 2, 3, 4, 5]
             .map(
-              (number) => Image.asset(
+              (number) =>
+              Image.asset(
                 'asset/img/image_$number.jpeg',
                 fit: BoxFit.cover,
               ),
-            )
+        )
             .toList(),
       ),
     );
