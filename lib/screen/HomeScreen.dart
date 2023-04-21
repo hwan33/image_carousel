04e8d7b5d0f9c406675f832c053a5effb0e64a8b
권용환 <hahahaa_a@naver.com>
2023-04-21 23:06:14 +0900

    상태바 색상 변경

--- lib/screen/HomeScreen.dart
+++ lib/screen/HomeScreen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
     return Scaffold(
       body: PageView(
         children: [1, 2, 3, 4, 5]
@@ -13,7 +15,7 @@ class HomeScreen extends StatelessWidget {
                 'asset/img/image_$number.jpeg',
                 fit: BoxFit.cover,
               ),
-        )
+            )
             .toList(),
       ),
     );
