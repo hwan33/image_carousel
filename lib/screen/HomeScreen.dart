f2835e15ab71534e35f2352e57ea44858fd7705c
권용환 <hahahaa_a@naver.com>
2023-04-21 23:20:58 +0900

    타이머 추가하기 - StatefulWidget

--- lib/screen/HomeScreen.dart
+++ lib/screen/HomeScreen.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
